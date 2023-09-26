8ca41b4ea7e82d2c9c100de9ac3c986989585159
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-26 09:35:01 +0700

    added another task

--- Praktek/Bitwise.java
+++ Praktek/Bitwise.java
@@ -4,16 +4,38 @@ public class Bitwise {
     public static void main(String[] args) {
         int x = 1;
         int y = 5;
+
         System.out.println((x > 1) & (x++ < 10));
+        // Same as
+        System.out.println(false & true);
+        System.out.println(0b00000000 & 0b00000001);
         x = 1;
         System.out.println((x > 1) && (x++ < 10));
+        // Same as
+        System.out.println(false && true);
+        // But can't do bitwise
+        // System.out.println(0b00000000 && 0b00000001);
         x = 1;
 
-        int binary1 = 0b00000010;
-        int binary2 = 0b00000110;
-        int result = 0b00000000;
-        System.out.println(binary1 & binary2);
-        System.out.println(2 & 5);
+        // Bitwise operator will covert integer to binary, then it will applly
+        // the operation to each bits
+
+        // Eg for and bitwise operator
+        int binaryOfX = 0b00000001;
+        int binaryOfY = 0b00000101;
+        int resultOfAnd = 0b00000001;
+        int resultOfOr = 0b00000101;
+
+        System.out.println(binaryOfX & binaryOfY);
+        // Same as
+        System.out.println(1 & 5);
+
+        // Another example
+        int a = 27;
+        int b = 74;
+        int binaryA = 0b00011011;
+        int binaryB = 0b01001010;
+        int result = 0b00001010;
         System.out.println(2 | 4);
 
         System.out.println((x > y) && (x < y));
