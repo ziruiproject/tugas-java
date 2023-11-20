be4ebb271863f53ffe396c66505f08bba4d35c23
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-11-20 12:11:35 +0700

    praktikum 5

--- Praktek/Praktikum5.java
+++ Praktek/Praktikum5.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Praktikum5 {
 
     public static void main(String[] args) {
+
+    }
+
+    private static void No1() {
         Scanner scanner = new Scanner(System.in);
 
         // Inisialisasi posisi awal kubu penyerang dan penghadang
@@ -56,4 +61,71 @@ public class Praktikum5 {
 
         scanner.close();
     }
+
+
+    private static final String[] kataOrang = { "Nathan", "Yesaya", "Takeshi", "Nanda" };
+    private static final String[] kataBendaUmum = { "pria", "wanita", "ikan", "gajah", "kuda" };
+    private static final String[] satuanBenda = { "seekor", "setiap", "beberapa", "seorang" };
+    private static final String[] kataSifat = { "besar", "kecil", "cantik", "botak" };
+    private static final String[] kataKerjaTidakTransitif = { "berlari", "melompat", "berbicara", "tidur" };
+    private static final String[] kataKerjaTransitif = { "mencintai", "membenci", "melihat", "mengetahui", "mencari",
+            "menemukan" };
+    private static final String[] konjungsi = { "dan", "atau", "tapi", "karena" };
+
+    public static void No2() {
+        Random random = new Random();
+        String kalimat = generateKalimat(random);
+        System.out.println(kalimat);
+    }
+
+    private static String generateKalimat(Random random) {
+        return capitalize(generateKalimatSederhana(random)) +
+                (random.nextDouble() < 0.5
+                        ? " " + konjungsi[random.nextInt(konjungsi.length)] + " " + generateKalimat(random)
+                        : "");
+    }
+
+    private static String generateKalimatSederhana(Random random) {
+        return generateFrasaBenda(random) + " " + generateFrasaKerja(random);
+    }
+
+    private static String generateFrasaBenda(Random random) {
+        if (random.nextDouble() < 0.5) {
+            return kataOrang[random.nextInt(kataOrang.length)];
+        } else {
+            return generateSatuanBenda(random) + (random.nextDouble() < 0.5 ? " " + generateKataSifat(random) : "") +
+                    ". " + kataBendaUmum[random.nextInt(kataBendaUmum.length)] +
+                    (random.nextDouble() < 0.5 ? " yang " + generateFrasaKerja(random) : "");
+        }
+    }
+
+    private static String generateFrasaKerja(Random random) {
+        if (random.nextDouble() < 0.25) {
+            return kataKerjaTidakTransitif[random.nextInt(kataKerjaTidakTransitif.length)];
+        } else if (random.nextDouble() < 0.5) {
+            return kataKerjaTransitif[random.nextInt(kataKerjaTransitif.length)] + " " + generateFrasaBenda(random);
+        } else if (random.nextDouble() < 0.75) {
+            return "adalah " + generateKataSifat(random);
+        } else {
+            return "meyakini bahwa " + generateKataSederhana(random);
+        }
+    }
+
+    private static String generateSatuanBenda(Random random) {
+        return satuanBenda[random.nextInt(satuanBenda.length)];
+    }
+
+    private static String generateKataSifat(Random random) {
+        return kataSifat[random.nextInt(kataSifat.length)];
+    }
+
+    private static String generateKataSederhana(Random random) {
+        return kataOrang[random.nextInt(kataOrang.length)] + " " + generateFrasaKerja(random);
+    }
+
+    private static String capitalize(String str) {
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+}
+
 }
